caecde3dacce6ff34d5d0f184f551c264f23901f
Makefile_dot_in <makefile.dot.am@gmail.com>
2025-11-27 17:28:11 +0200

    Add logging

--- main.rkt
+++ main.rkt
@@ -4,6 +4,8 @@
          (prefix-in files:     web-server/dispatchers/dispatch-files)
          (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
          (prefix-in filter:    web-server/dispatchers/dispatch-filter)
+         (prefix-in logresp:   web-server/dispatchers/dispatch-logresp)
+         
          web-server/dispatchers/filesystem-map
          web-server/servlet-env
          web-server/web-server)
@@ -14,8 +16,14 @@
       (files:make #:url->path _)
       (filter:make #rx"^/static/" _)))
 
+(define dispatcher
+  (~> (sequencer:make files)
+      (logresp:make #:log-path (current-output-port)
+                    #:format 'apache-default
+                    _)))
+
 (define stop
-  (serve #:dispatch (sequencer:make files)
+  (serve #:dispatch dispatcher
          #:port 8080))
 
 (with-handlers ([exn:break? (λ (e) (stop))])
