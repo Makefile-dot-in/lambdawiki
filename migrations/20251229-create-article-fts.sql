6fc70de7fbfbe543375a2a1a72bfa7f002f044dc
Makefile_dot_in <makefile.dot.am@gmail.com>
2025-12-29 17:35:44 +0200

    Fix FTS issue

--- migrations/20251229-create-article-fts.sql
+++ migrations/20251229-create-article-fts.sql
@@ -5,7 +5,7 @@
 -- @description: Creates a full text index for article names.
 -- @up {
 create index article_name_fts on articles
-using gin (to_tsvector(name));
+using gin (to_tsvector('english', name));
 -- }
 
 -- @down {
